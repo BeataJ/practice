bc96ac4365e9309f06c42fa02296f6610536bc85
Beata <beata@jasinski.com>
2024-04-24 21:20:14 -0700

    array practice

--- practice.go
+++ practice.go
@@ -1,6 +1,70 @@
 package main
 
-func main() {}
+import "fmt"
+
+type Product struct {
+	title string
+	price float64
+	id    int
+}
+
+func main() {
+	// 1
+	hobbies := [3]string{"cooking", "reading", "coding"}
+	fmt.Println(hobbies)
+
+	// 2
+	fmt.Println(hobbies[0])
+	new_hobbies := hobbies[1:]
+	fmt.Println(new_hobbies)
+
+	// 3
+	slice_hobbies := hobbies[:2]
+	slice_hobbies2 := hobbies[0:2]
+	fmt.Println(slice_hobbies)
+	fmt.Println(slice_hobbies2)
+	fmt.Println(cap(slice_hobbies))
+
+	// 4
+	slice_hobbies = slice_hobbies[1:3]
+	fmt.Println(slice_hobbies)
+
+	// 5
+	dynamicArray := []string{"coding Go", "Be good in go"}
+	fmt.Println(dynamicArray)
+
+	// 6
+	dynamicArray[1] = "Go Go Go"
+	fmt.Println(dynamicArray)
+	addSlice := "new goal"
+	dynamicArray = append(dynamicArray, addSlice)
+
+	fmt.Println(dynamicArray)
+
+	// 7
+	products := []Product{
+		{
+			title: "glass",
+			price: 8.99,
+			id:    1,
+		},
+		{
+			title: "bottle",
+			price: 99,
+			id:    2,
+		},
+	}
+
+	fmt.Println(products)
+	p := Product{
+		"stone",
+		3.99,
+		3,
+	}
+
+	products = append(products, p)
+	fmt.Println(products)
+}
 
 // Time to practice what you learned!
 
